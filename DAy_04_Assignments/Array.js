d4f842191ebc740399e367da35c57c4844dd7537
AJAY <98752820+ajay9679339@users.noreply.github.com>
2022-02-11 14:47:46 +0530

    DAY_04_Assignment_Submited

---
+++ DAy_04_Assignments/Array.js
@@ -0,0 +1,39 @@
+var avengers=[
+    {
+        name:"Thor",
+        weapon :"mjolnir",
+        age:"1500 years",
+        realname:"Chris Hemsworth",
+    },
+    {
+        name:"Captain America",
+        weapon :"Sheid",
+        age:"90 years",
+        realname:"Christopher George Sarris",
+    },
+    {
+        name:"Iron Man",
+        weapon :"Armour",
+        age:"34 years",
+        realname:"Tony Stark",
+    },
+    {
+        name:"SpiderMan",
+        weapon :"Web Shooter",
+        age:"28 years",
+        realname:"Peter Parker",
+    },
+    {
+        name:"Doctor Strange",
+        weapon :"Infinity Stones",
+        age:"42 years",
+        realname:"Peter Hooten",
+    }
+];
+
+for(var i=0;i<avengers.length;i++)
+{
+    console.log(avengers[i].name,avengers[i].age);
+}
+
+
---
+++ DAy_04_Assignments/forEach.js
@@ -0,0 +1,41 @@
+var avengers=[
+    {
+        name:"Thor",
+        weapon :"mjolnir",
+        age:"1500 years",
+        realname:"Chris Hemsworth",
+    },
+    {
+        name:"Captain America",
+        weapon :"Sheid",
+        age:"90 years",
+        realname:"Christopher George Sarris",
+    },
+    {
+        name:"Iron Man",
+        weapon :"Armour",
+        age:"34 years",
+        realname:"Tony Stark",
+    },
+    {
+        name:"SpiderMan",
+        weapon :"Web Shooter",
+        age:"28 years",
+        realname:"Peter Parker",
+    },
+    {
+        name:"Doctor Strange",
+        weapon :"Infinity Stones",
+        age:"42 years",
+        realname:"Peter Hooten",
+    }
+];
+
+avengers.forEach(
+    function(avengers,index)
+    {
+        console.log(avengers,index);
+    }
+)
+
+
